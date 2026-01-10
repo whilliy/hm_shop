d813da845a735d33302bb5e17e42cf2a71d55a0a
Li Yongchun <return@126.com>
2026-01-10 12:34:29 +0800

    搭建基本框架

---
+++ lib/pages/Main/index.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('首页')),
+      body: Center(child: Text('首页')),
+    );
+  }
+}
